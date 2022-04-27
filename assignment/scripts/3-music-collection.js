704f0c490401c2cbb9f0678d7691a1774ca9f0fe
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-04-27 14:29:50 -0500

    newObject created

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,5 +1,10 @@
 console.log('***** Music Collection *****')
 let collection = [];
    function addToCollection(title, artist, yearPublished) {
-    
+   let newObject = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+   }
+   return collection.push(newObject);
 }
