d599dd01aaf5e95a7775de9a004175bf57036782
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-04-27 15:02:50 -0500

    6 albums added

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -8,3 +8,9 @@ let collection = [];
    }
    return collection.push(newObject);
 }
+addToCollection('The Divine Feminine','Mac Miller',2016);
+addToCollection('Bobby Tarantino ||','Logic',2018);
+addToCollection('Positions','Ariana Grande',2020);
+addToCollection('Future Nostalgia','Dua Lipa',2020);
+addToCollection('Tickets To My Downfall','MGK',2020)
+addToCollection('Happier Than Ever','Billie Eilish',2021);
