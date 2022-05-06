b80ab8131a6b0c5e86dd83dac2f06f5d7a6a7fe9
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-05-06 15:03:12 -0500

    Fixed findByArtist function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,29 +1,34 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
-   
+
 function addToCollection(title, artist, yearPublished, name, duration) {
-   let newAlbum = {
-    title: title,
-    artist: artist,
-    yearPublished: yearPublished,
-    tracks: { name: name, duration: duration }
-   }
-   return collection.push(newAlbum);
+    
+    let newAlbum = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished,
+        tracks: { name: name, duration: duration }
+    }
+    collection.push(newAlbum);
+
+    return newAlbum;
 }
 
 
-addToCollection('The Divine Feminine','Mac Miller',2016,'Congratulations','4:16');
+addToCollection('The Divine Feminine', 'Mac Miller', 2016, 'Congratulations', '4:16');
 
-addToCollection('Bobby Tarantino ||','Logic',2018,'Warm It Up','4:00');
+addToCollection('Circles', 'Mac Miller', 2020,);
 
-addToCollection('Positions','Ariana Grande',2020,'34+35','2:30');
+addToCollection('Bobby Tarantino ||', 'Logic', 2018, 'Warm It Up', '4:00');
 
-addToCollection('Future Nostalgia','Dua Lipa',2020);
+addToCollection('Positions', 'Ariana Grande', 2020, '34+35', '2:30');
 
-addToCollection('Tickets To My Downfall','MGK',2020)
+addToCollection('Future Nostalgia', 'Dua Lipa', 2020);
 
-addToCollection('Happier Than Ever','Billie Eilish',2021);
+addToCollection('Tickets To My Downfall', 'MGK', 2020);
+
+addToCollection('Happier Than Ever', 'Billie Eilish', 2021);
 
 
 console.log(collection[0]);
@@ -40,47 +45,54 @@ console.log(collection[5]);
 
 console.log(collection);
 
-    function showCollection(array) {
-        console.log('In showCollection');
-        console.log('There are:', array.length,'albums in your collection');
+function showCollection(array) {
 
-        for (let i=0; i<array.length; i++) {
-            console.log(array[i].title,'by',array[i].artist,'published in',array[i].yearPublished);
-        }
-        return true;
+    console.log('In showCollection');
+
+    console.log('There are:', array.length, 'albums in your collection');
+
+    for (let i = 0; i < array.length; i++) {
+
+        console.log(array[i].title, 'by', array[i].artist, 'published in', array[i].yearPublished);
     }
+    return true;
+}
 
-    showCollection(collection);
+showCollection(collection);
 
 
+function findByArtist(artist) {
     let results = [];
 
-    function findByArtist(artist) {
-        for (result of collection) {
-            if (artist === result.artist) {
-                results.push(result);
-                return false;
-            }else{
-                results.pop();
-                console.log('searching...');
-            }
-        }
-        return results;
-        }  
-        
+    for (newAlbum of collection) {
 
-findByArtist(  'Billie Eilish'  );
+        if (newAlbum.artist === artist) {
+
+            results.push(newAlbum);
+        } else {
+            //do nothing
+        }
+    }
+    return results;
+}
+
+
+findByArtist('Billie Eilish');
 
 let searchResults = [];
-  
-function search (artist,yearPublished,trackName) {
-            for (album of collection)
-            if (album.artist === artist && album.yearPublished === yearPublished || trackName === album.tracks) {
-                searchResults.push(album);
-                return searchResults;
-            }
-            return collection;
+
+function search(artist, yearPublished, trackName) {
+
+    for (album of collection)
+
+        if (album.artist === artist && album.yearPublished === yearPublished || trackName === album.tracks) {
+            
+            searchResults.push(album);
+            
+            return searchResults;
         }
+    return collection;
+}
 /* Attempt at adding tracks
 
 const getNestedObject = (nestedObj, pathArr) =>{
@@ -93,4 +105,3 @@ const name = getNestedObject(newAlbum, ['tracks', 0,'name']); */
 
 
 
-       
